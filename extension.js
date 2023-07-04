c80eae2aa808197a06f9d855656e3b57363c0f12
Svante Boberg <svante.boberg@gmail.com>
2023-07-04 21:09:43 +0200

    Format with prettier

--- extension.js
+++ extension.js
@@ -1,52 +1,62 @@
-const os = require('node:os');
-const { mkdtemp, readFile, rm } = require('node:fs/promises');
-const vscode = require('vscode');
+const os = require("node:os");
+const { mkdtemp, readFile, rm } = require("node:fs/promises");
+const vscode = require("vscode");
 
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('fzf', async function () {
-		const tempDir = await mkdtemp("/tmp/");
-		const tempPath = `${tempDir}/fzf.out`;
+  let disposable = vscode.commands.registerCommand("fzf", async function () {
+    const tempDir = await mkdtemp("/tmp/");
+    const tempPath = `${tempDir}/fzf.out`;
 
-		const terminal = vscode.window.createTerminal({
-			hideFromUser: true,
-			name: 'fzf',
-			shellPath: 'zsh',
-			shellArgs: ['-c', `pwd > ${tempPath} && rg --line-number --no-heading --color=always -g '!yarn.lock' '' | fzf --ansi --print0 --delimiter : --preview 'batcat --style=numbers --color=always --highlight-line {2} {1}' --preview-window +{2}-/2 >> ${tempPath}`],
-		});
+    const terminal = vscode.window.createTerminal({
+      hideFromUser: true,
+      name: "fzf",
+      shellPath: "zsh",
+      shellArgs: [
+        "-c",
+        `pwd > ${tempPath} && rg --line-number --no-heading --color=always -g '!yarn.lock' '' | fzf --ansi --print0 --delimiter : --preview 'batcat --style=numbers --color=always --highlight-line {2} {1}' --preview-window +{2}-/2 >> ${tempPath}`,
+      ],
+    });
 
-		terminal.show();
+    terminal.show();
 
-		const listener = vscode.window.onDidChangeActiveTerminal(async () => {
-			if (terminal.exitStatus !== undefined) {
-				const lines = (await readFile(tempPath, 'utf8')).split('\n');
-				listener.dispose();
-				await rm(tempDir, { recursive: true });
+    const listener = vscode.window.onDidChangeActiveTerminal(async () => {
+      if (terminal.exitStatus !== undefined) {
+        const lines = (await readFile(tempPath, "utf8")).split("\n");
+        listener.dispose();
+        await rm(tempDir, { recursive: true });
 
-				if (!lines[1]) {
-					// I.e. ctrl-c or similar
-					return;
-				}
+        if (!lines[1]) {
+          // I.e. ctrl-c or similar
+          return;
+        }
 
-				const pwd = lines[0];
-				const parts = lines[1].split(':');
-				const filePath = `${pwd}/${parts[0]}`;
-				const lineNumber = parseInt(parts[1]);
+        const pwd = lines[0];
+        const parts = lines[1].split(":");
+        const filePath = `${pwd}/${parts[0]}`;
+        const lineNumber = parseInt(parts[1]);
 
-				const editor = await vscode.window.showTextDocument(vscode.Uri.file(filePath));
-				const line = editor.document.lineAt(lineNumber - 1)
-				const column = line.firstNonWhitespaceCharacterIndex;
-				editor.selection =  new vscode.Selection(lineNumber - 1, column, lineNumber - 1, column);
-				editor.revealRange(line.range);
-			}
-		})
-	});
+        const editor = await vscode.window.showTextDocument(
+          vscode.Uri.file(filePath)
+        );
+        const line = editor.document.lineAt(lineNumber - 1);
+        const column = line.firstNonWhitespaceCharacterIndex;
+        editor.selection = new vscode.Selection(
+          lineNumber - 1,
+          column,
+          lineNumber - 1,
+          column
+        );
+        editor.revealRange(line.range);
+      }
+    });
+  });
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-function deactivate() { }
+function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
